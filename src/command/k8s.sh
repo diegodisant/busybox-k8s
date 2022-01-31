a2760c8eab7c3c928d4d203c6250fc1d4c2e4888
diegodisant <diego.desantiago@linio.com>
2022-01-31 14:39:30 -0600

    feat: add attach commands

--- src/command/k8s.sh
+++ src/command/k8s.sh
@@ -22,6 +22,24 @@ set_country_ar() {
   export COUNTRY_DEFAULT=${COUNTRY_CL}
 }
 
+attach_sh_shell() {
+  kubectl exec \
+    -n ${NAMESPACE_DEFAULT} \
+    --stdin \
+    --tty \
+    ${1} \
+    -- /bin/sh
+}
+
+attach_bash_shell() {
+  kubectl exec \
+    -n ${NAMESPACE_DEFAULT} \
+    --stdin \
+    --tty \
+    ${1} \
+    -- /bin/bash
+}
+
 alias k8s-cluster3-staging="aws eks update-kubeconfig --region us-east-1 --name sc-cluster-linio3-staging"
 alias k8s-cluster4-staging="aws eks update-kubeconfig --region us-east-1 --name sc-cluster-linio4-staging"
 alias k8s-cluster3-live="aws eks update-kubeconfig --region us-east-1 --name sc-cluster-linio3-live"
@@ -36,8 +54,10 @@ alias k8s-switch-ar="set_country_ar"
 alias k8s-hpa="kubectl get hpa -n ${NAMESPACE_DEFAULT}"
 alias k8s-pods-all="kubectl get pods --kubeconfig ~/.kube/config -A"
 alias k8s-pods="kubectl get pods --kubeconfig ~/.kube/config -n ${NAMESPACE_DEFAULT}"
-alias k8s-pods-create="kubectl pods create --kubeconfig ~/.kube/config -n ${NAMESPACE_DEFAULT}"
-alias k8s-pods-delete="kubectl pods delete --kubeconfig ~/.kube/config -n ${NAMESPACE_DEFAULT}"
+alias k8s-pods-create="kubectl create pods --kubeconfig ~/.kube/config -n ${NAMESPACE_DEFAULT}"
+alias k8s-pods-delete="kubectl delete pods --kubeconfig ~/.kube/config -n ${NAMESPACE_DEFAULT}"
 alias k8s-check-cron-force-sync="kubectl get pods --kubeconfig ~/.kube/config -n ${NAMESPACE_DEFAULT} | grep 'cron-force-sync'"
-alias k8s-check-worker-image="kubectl get pods --kubeconfig ~/.kube/config -n ${NAMESPACE_DEFAULT} | grep 'worker-update-image"
-alias k8s-create-cron-force-sync="kubectl create job --from=cronjob/${NAMESPACE_DEFAULT}-cron-force-sync-image -n ${NAMESPACE_DEFAULT} ${NAMESPACE_DEFAULT}-cron-force-sync-image-$(date | sha1sum | tr -d ' -')"
+alias k8s-check-worker-image="kubectl get pods --kubeconfig ~/.kube/config -n ${NAMESPACE_DEFAULT} | grep 'worker-update-image'"
+alias k8s-create-cron-force-sync="kubectl create job --from=cronjob/${NAMESPACE_DEFAULT}-cron-force-sync-image -n ${NAMESPACE_DEFAULT} ${NAMESPACE_DEFAULT}-cron-force-sync-image-`date | sha1sum | tr -d ' -' | tr -d '\n' | cut -b1-10`"
+alias k8s-pod-shell-bash="attach_bash_shell"
+alias k8s-pod-shell-sh="attach_sh_shell"
